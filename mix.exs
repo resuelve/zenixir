c516490e6e797382cefb69c1a65b1aa2d7eabbdc
Poncho Martinez <poncho.811@gmail.com>
2017-10-24 15:46:50 -0500

    Mix fixes

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule NewTest.Mixfile do
+defmodule Zenixir.Mixfile do
   use Mix.Project
 
   def project do
     [app: :zenixir,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
